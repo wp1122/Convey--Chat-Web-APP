0502c456d7bf7029bf4e3819dbb3e7064c6c3784
Ayush Chaudhary <acayush2002@gmail.com>
2022-09-18 00:29:48 +0530

    chatbox nav

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import videoIcon from "../images/cam.png";
+import addFriend from "../images/add.png";
+import moreSettings from "../images/more.png";
 
 const Chatbox = () => {
   return (
-    <div className='chatbox'>Chatbox</div>
+    <div className='chatbox'>
+      <div className='chatInfo'>
+        <span>Nancy</span>
+        <div className="chatIcons">
+          <img src={videoIcon} alt="videocall" />
+          <img src={addFriend} alt="Add friend" />
+          <img src={moreSettings} alt="More settings" />
+        </div>
+      </div>
+    </div>
   )
 }
 
