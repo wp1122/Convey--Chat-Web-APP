89fd97dcb6b9ab172e388a7c0eb6bf2b0456c1d9
Ayush Chaudhary <acayush2002@gmail.com>
2022-09-18 03:26:27 +0530

    chatbox completed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import profilepic from "../images/pfp.jpg";
 const Message = () => {
-  return (
-    <div>
-      Message
+  return ( 
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src={profilepic} alt="pfp" />
+        <span>
+          Just now
+        </span>
+      </div>
+      <div className="messageContent">
+        <p>heyy</p>
+        <img src={profilepic} alt="pfp2" />
+      </div>
     </div>
   )
 }
